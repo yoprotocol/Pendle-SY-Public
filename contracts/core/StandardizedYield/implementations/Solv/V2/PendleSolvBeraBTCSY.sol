0deba5d6653953026ef58a4bf80604cf95d6bacc
ngfam <ngfamgithub@gmail.com>
2025-01-15 14:35:00 +0700

    Added initialization for PendleSolvBeraBTCSY

--- contracts/core/StandardizedYield/implementations/Solv/V2/PendleSolvBeraBTCSY.sol
+++ contracts/core/StandardizedYield/implementations/Solv/V2/PendleSolvBeraBTCSY.sol
@@ -11,9 +11,17 @@ contract PendleSolvBeraBTCSY is PendleSolvSYBaseV2 {
     address public constant SOLV_BBN_BTC = 0xd9D920AA40f578ab794426F5C90F6C731D159DEf;
     address public constant SOLV_BERA_BTC = 0xE7C253EAD50976Caf7b0C2cbca569146A7741B50;
 
-    address[] internal FULL_PATH = ArrayLib.create(WBTC, SOLV_BTC, SOLV_BBN_BTC, SOLV_BERA_BTC);
+    address[] internal FULL_PATH;
 
     constructor() PendleSolvSYBaseV2(SOLV_BTC_ROUTER_V2, SOLV_BERA_BTC) {}
+
+    function initialize(string memory _name, string memory _symbol) external initializer {
+        __SYBaseUpg_init(_name, _symbol);
+        _safeApproveInf(WBTC, SOLV_BTC_ROUTER_V2);
+        _safeApproveInf(SOLV_BTC, SOLV_BTC_ROUTER_V2);
+        _safeApproveInf(SOLV_BBN_BTC, SOLV_BTC_ROUTER_V2);
+        FULL_PATH = ArrayLib.create(WBTC, SOLV_BTC, SOLV_BBN_BTC, SOLV_BERA_BTC);
+    }
 
     function _previewDeposit(
         address tokenIn,
@@ -22,7 +30,7 @@ contract PendleSolvBeraBTCSY is PendleSolvSYBaseV2 {
         for (uint256 i = 0; i + 1 < FULL_PATH.length; ++i) {
             if (tokenIn == FULL_PATH[i]) {
                 address nxtToken = FULL_PATH[i + 1];
-                bytes32 poolId = ISolvRouterV2(solvRouterV2).poolIds(tokenIn, nxtToken);
+                bytes32 poolId = ISolvRouterV2(solvRouterV2).poolIds(nxtToken, tokenIn);
                 (tokenIn, amountTokenToDeposit) = (nxtToken, _previewSolvConvert(poolId, amountTokenToDeposit));
             }
         }
