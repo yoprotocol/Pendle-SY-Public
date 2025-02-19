f600f4950354e1f9e2d69bae2f68dee00885401c
ngfam <ngfamgithub@gmail.com>
2025-02-19 15:07:03 +0700

    Added usdf minting from usd

--- contracts/core/StandardizedYield/implementations/Astherus/PendleAstherusASUSDFSY.sol
+++ contracts/core/StandardizedYield/implementations/Astherus/PendleAstherusASUSDFSY.sol
@@ -5,6 +5,9 @@ import "../../SYBaseUpg.sol";
 import "../../../../interfaces/Astherus/IAstherusEarn.sol";
 
 contract PendleAstherusASUSDFSY is SYBaseUpg {
+    address public constant BINANCE_USD = 0x55d398326f99059fF775485246999027B3197955;
+    address public constant USDF_EARN = 0xC271fc70dD9E678ac1AB632f797894fe4BE2C345;
+
     address public constant ASUSDF = 0x917AF46B3C3c6e1Bb7286B9F59637Fb7C65851Fb;
     address public constant USDF = 0x5A110fC00474038f6c02E89C707D638602EA44B5;
     address public constant EARN = 0xdB57a53C428a9faFcbFefFB6dd80d0f427543695;
@@ -14,6 +17,11 @@ contract PendleAstherusASUSDFSY is SYBaseUpg {
     function initialize() external initializer {
         __SYBaseUpg_init("SY Astherus asUSDF", "SY-asUSDF");
         _safeApproveInf(USDF, EARN);
+        _safeApproveInf(BINANCE_USD, USDF_EARN);
+    }
+
+    function approveUsdfEarn() external onlyOwner {
+        _safeApproveInf(BINANCE_USD, USDF_EARN);
     }
 
     /*///////////////////////////////////////////////////////////////
@@ -24,6 +32,10 @@ contract PendleAstherusASUSDFSY is SYBaseUpg {
         address tokenIn,
         uint256 amountDeposited
     ) internal virtual override returns (uint256 /*amountSharesOut*/) {
+        if (tokenIn == BINANCE_USD) {
+            IAstherusEarn(USDF_EARN).deposit(amountDeposited);
+            (tokenIn, amountDeposited) = (USDF, _selfBalance(USDF));
+        }
         if (tokenIn != ASUSDF) {
             uint256 preBalance = _selfBalance(ASUSDF);
             IAstherusEarn(EARN).deposit(amountDeposited);
@@ -69,7 +81,7 @@ contract PendleAstherusASUSDFSY is SYBaseUpg {
     }
 
     function getTokensIn() public pure override returns (address[] memory res) {
-        return ArrayLib.create(USDF, ASUSDF);
+        return ArrayLib.create(USDF, ASUSDF, BINANCE_USD);
     }
 
     function getTokensOut() public pure override returns (address[] memory res) {
@@ -77,7 +89,7 @@ contract PendleAstherusASUSDFSY is SYBaseUpg {
     }
 
     function isValidTokenIn(address token) public pure override returns (bool) {
-        return token == USDF || token == ASUSDF;
+        return token == USDF || token == ASUSDF || token == BINANCE_USD;
     }
 
     function isValidTokenOut(address token) public pure override returns (bool) {
